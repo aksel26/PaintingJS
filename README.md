5387c4a20f98280bd757b3ecd683f6a377da183f
aksel26 <kevinxkim2023@gmail.com>
2020-09-14 16:25:58 +0900

    js Canvas Done

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # PaintingJS
+
+- 캔버스 API 
+
+  [MDN사이트](https://developer.mozilla.org/ko/docs/Web/HTML/Canvas)에 들어간다.
+
+- 기본적으로 canvas는 html의 한 요소
+
+  - 다른점은 context를 갖는다는 것.
+  - context라는 요소 안에서 픽셀에 접근할 수 있는 방법
+
+- Paths
+- StrokeStyle
+- lineWidth
+
--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext('2d');
+
+// CSS에 사이즈를 지정한 것 말고 실제 캔버스 크기를 지정해 주어야 한다.
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "black";
+ctx.lineWidth = 2.5;
+
 let painting = false;
 
 function stopPainting() {
     painting = false;
+}
+
+function startPainting() {
+    painting = true;
 }
 function onMouseMove(event) {
 
@@ -15,21 +28,25 @@ function onMouseMove(event) {
     const y = event.offsetY;
 
     // 클릭하는 순간을 인지하고 painting을 시작해야 한다.
-
+    if (!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
- 
+
 
 function onMousedown(event) {
     painting = true;
 }
 
-function onMouseUp(event) {
-    stopPainting();
-}
-
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMousedown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+
